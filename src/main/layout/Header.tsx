acb069b96d7ab3611b54323715a3d83c70dbf9d3
joey <786678257@qq.com>
2025-04-25 18:02:38 +0800

    chore

--- src/main/layout/Header.tsx
+++ src/main/layout/Header.tsx
@@ -1,10 +1,12 @@
 export default function Header() {
   return (
     <header className="titleBarContainer">
-      <div className="titleBar header">
-        <span>ElectronViteTemplate</span>
-        <div className="draggable draggable-area"></div>
-        {import.meta.env.DEV && <button onClick={window.mainAPI.toggleDevtools}>devtools</button>}
+      <div className="titleBar">
+        <div className="header">
+          <span>ElectronViteTemplate</span>
+          <div className="draggable draggable-area"></div>
+          {import.meta.env.DEV && <button onClick={window.mainAPI.toggleDevtools}>devtools</button>}
+        </div>
       </div>
     </header>
   );
